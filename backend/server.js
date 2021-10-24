074151da88e1fb2afe1e9b0b593d9b2bab013c4b
Muhammad5943 <muhammad.aji.putra.mp98@gmail.com>
2021-10-24 18:52:30 +0700

    configure redux in ui

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,20 @@ app.get('/api/products', (req, res) => {
      })
 })
 
+app.get('/api/product/:_id', (req, res) => {
+     console.log('data ', data.products);
+     const product = data.products.find((x) => x._id === req.params._id)
+     console.log('productId ', req.params._id);
+     console.log('product ', product);
+     if (product) {
+          res.send(product)
+     } else {
+          res.status(404).send({
+               message: 'Product not found'
+          })
+     }
+})
+
 const port = process.env.PORT
 
 app.listen(port, () => {
